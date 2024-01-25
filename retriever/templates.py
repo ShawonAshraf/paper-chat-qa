a052fe99ecd55e40c5253e97216a0d557004a693
Shawon Ashraf <shawon13@live.com>
2024-01-25 19:23:23 +0100

    change prompt for retreival qa

--- retriever/templates.py
+++ retriever/templates.py
@@ -2,18 +2,17 @@ from langchain_core.prompts import ChatPromptTemplate
 
 """
 Prepares a prompt template with a default system message
-system_message: str - for a custom system message
 """
 
 
-def prepare_template(system_message: str = "") -> ChatPromptTemplate:
-    sys_message = "You are an expert at summarising research papers." if system_message == "" else system_message
+def prepare_template() -> ChatPromptTemplate:
+    prompt = ChatPromptTemplate.from_template("""Answer the following question based only on the provided context:
 
-    prompt = ChatPromptTemplate.from_messages([
-        ("system", sys_message),
-        ("user", "{input}")
-    ]
-    )
+    <context>
+    {context}
+    </context>
+
+    Question: {input}""")
 
     return prompt
 
