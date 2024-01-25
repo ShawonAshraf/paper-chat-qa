1fe9e6bc736491534cfcef5fc6a04f681cab210b
Shawon Ashraf <shawon13@live.com>
2024-01-26 00:22:27 +0100

    bootstrap

--- web/src/app/app.tsx
+++ web/src/app/app.tsx
@@ -1,10 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-
+import "bootstrap/dist/css/bootstrap.min.css";
 import FileUploader from "./components/fileuploader";
 
 export function App() {
     return (
-        <div>
+        <div className="container" style={{ width: "600px" }}>
+            <div className="my-4">
+                <h1>Paper Summariser</h1>
+            </div>
             <FileUploader />
         </div>
     );
