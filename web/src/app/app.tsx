fbd09dc613141e85a8e71e1f7f48b868c69056d1
Shawon Ashraf <shawon13@live.com>
2024-01-26 01:44:22 +0100

    query frame component complete

--- web/src/app/app.tsx
+++ web/src/app/app.tsx
@@ -1,14 +1,17 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import "bootstrap/dist/css/bootstrap.min.css";
 import FileUploader from "./components/fileuploader";
+import QueryFrame from "./components/query";
+
 
 export function App() {
     return (
-        <div className="container" style={{ width: "600px" }}>
+        <div className="container" style={{ width: "800px" }}>
             <div className="my-4">
                 <h1>Paper Summariser</h1>
             </div>
             <FileUploader />
+            <QueryFrame />
         </div>
     );
 }
--- web/src/app/components/query.tsx
+++ web/src/app/components/query.tsx
@@ -1,29 +1,77 @@
+import { useState } from "react";
+import HttpRequestService from "../utils/requests";
+
 const QueryFrame: React.FC = () => {
+    const [currentQuery, setCurrentQuery] = useState<string>();
+    const [currentResponse, setCurrentResponse] = useState<string>();
+
+    // handle changes in the query text area
+    // https://bobbyhadz.com/blog/react-get-textarea-value
+    const handleTextAreaChanges = (event: Event) => {
+        setCurrentQuery(event.target?.value);
+    };
+
+    // send query
+    const sendQuery = () => {
+        if (!currentQuery) return;
+
+        HttpRequestService.sendQuery(currentQuery)
+            .then((response) => {
+                setCurrentResponse(response.data.results);
+            })
+            .catch((err) => {
+                if (
+                    err.response &&
+                    err.response.data &&
+                    err.response.data.message
+                ) {
+                    setCurrentResponse(err.response.data.message);
+                } else {
+                    setCurrentResponse(
+                        "Error getting a result from the LLM. Try restarting the API server."
+                    );
+                }
+            });
+    };
+
     return (
         <div className="my-5">
             {/* textarea for query input */}
             <div className="form-group">
-                <h2>Query here</h2>
-                <form>
-                    <div className="form-outline">
-                        <textarea
-                            className="form-control"
-                            id="textAreaExample1"
-                            rows="5"
-                        ></textarea>
-                    </div>
-                </form>
+                <h2>Query</h2>
+                <div className="form-outline">
+                    <textarea
+                        className="form-control"
+                        id="queryTextArea"
+                        rows="5"
+                        onChange={handleTextAreaChanges}
+                    ></textarea>
+                </div>
+
+                <div className="col-5">
+                    <button
+                        className="btn btn-success btn-sm"
+                        disabled={!currentQuery}
+                        onClick={sendQuery}
+                    >
+                        Send
+                    </button>
+                </div>
             </div>
 
             {/* response */}
             <div className="my-4">
                 <h2>Response</h2>
-                <p>
-                    <em>Response goes here!</em>
-                </p>
+                <div className="my-4">
+                    <h6>Your Query : {currentQuery}</h6>
+                    <h6>LLAMA2 : </h6>
+                    <p id="responseBody">
+                        <em>{currentResponse}</em>
+                    </p>
+                </div>
             </div>
         </div>
     );
-}
+};
 
 export default QueryFrame;
