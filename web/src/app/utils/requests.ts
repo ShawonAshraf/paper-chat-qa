a7560aa9c327273fa8a196974c1a27ec786fa24f
Shawon Ashraf <shawon13@live.com>
2024-02-03 14:43:33 +0100

    update url

--- web/src/app/utils/requests.ts
+++ web/src/app/utils/requests.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-    baseURL: "http://localhost:8000",
+    baseURL: process.env.API_URL|| "http://localhost:8000",
     headers: {
         "Content-type": "application/json",
     },
