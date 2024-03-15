15d3849a5af3f8d5a4ce308d4ac3ce84bee2fd93
ShawonAshraf <shawon13@live.com>
2024-03-15 04:55:57 +0100

    change api port in requests.ts

--- web/src/app/utils/requests.ts
+++ web/src/app/utils/requests.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-    baseURL: process.env.API_URL|| "http://localhost:8000",
+    baseURL: process.env.API_URL|| "http://localhost:9000",
     headers: {
         "Content-type": "application/json",
     },
